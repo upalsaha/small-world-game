fa7a1f606a33f5b1eeb09f1d3971c93af096bcfa
Tyler <tlr8cn@virginia.edu>
2017-04-21 23:01:03 -0400

    new stuff

---
+++ Lil' Boys Don't Cry/Assets/Scripts/MomAndPop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MomAndPop : MonoBehaviour {
+
+	public GameObject textBox;
+	private GUIStyle style = new GUIStyle();
+	public string[] textLines;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI(){
+
+		style.fontSize = 20;
+		GUI.Label(new Rect(300, 500, 100, 20), textLines[0], style);
+ 	}
+}
