98412fed2218c4194cb0cd415e2e70b9df87c80a
Tyler <tlr8cn@virginia.edu>
2017-04-22 01:28:35 -0400

    shart

--- Lil' Boys Don't Cry/Assets/Scripts/MomAndPop.cs
+++ Lil' Boys Don't Cry/Assets/Scripts/MomAndPop.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 public class MomAndPop : MonoBehaviour {
@@ -8,19 +9,59 @@ public class MomAndPop : MonoBehaviour {
 	private GUIStyle style = new GUIStyle();
 	public string[] textLines;
 
+	public string textToDisplay;
+
+	public GameObject shart;
+	bool shartDisplayed;
+
+
+
+	int counter;
+
 	// Use this for initialization
 	void Start () {
-		
+		counter = 0;
+		textToDisplay = textLines[0];
+		textToDisplay = textToDisplay.Replace("@", System.Environment.NewLine);
+		style.fontSize = 20;
+
+		shartDisplayed = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if(counter == textLines.Length - 2) {
+			style.fontSize = 24;
+			style.fontStyle = FontStyle.Bold;
+		} else if(counter == textLines.Length -1 && !shartDisplayed) {
+			Instantiate(shart);
+			shartDisplayed = true;
+		}
+
+		if (Input.GetKeyDown(KeyCode.E)) {
+			if(counter < textLines.Length -1 ) {
+				counter++;
+    			textToDisplay = textLines[counter];
+    			textToDisplay = textToDisplay.Replace("@", System.Environment.NewLine);
+
+    			
+    		} else {
+    			textToDisplay = "";
+    		}
+		}
 	}
 
 	void OnGUI(){
+		
+		
+		GUI.Label(new Rect(210, 500, 100, 20), textToDisplay, style);
 
-		style.fontSize = 20;
-		GUI.Label(new Rect(300, 500, 100, 20), textLines[0], style);
+
  	}
+
+ 	void OnMouseDown()
+    {
+    	
+    }
 }
